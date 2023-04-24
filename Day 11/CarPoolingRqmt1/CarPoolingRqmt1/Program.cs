b01cc94f7ca9867033a7abccc9fa7ba3f232e9a3
NabeelaMK <129931417+NabeelaMK@users.noreply.github.com>
2023-04-24 09:08:39 +0530

    Add files via upload

---
+++ Day 11/CarPoolingRqmt1/CarPoolingRqmt1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPoolingReq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Member> list = new List<Member>();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Member {0}:", i + 1);
+                Console.Write("Id: ");
+                long id = long.Parse(Console.ReadLine());
+                Console.Write("First Name: ");
+                string firstName = Console.ReadLine();
+                Console.Write("Last Name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Contact Number: ");
+                string contactNumber = Console.ReadLine();
+                Console.Write("License Number: ");
+                string licenseNumber = Console.ReadLine();
+                Console.Write("License Start Date (dd-MM-yyyy): ");
+                DateTime licenseStartDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
+                Console.Write("License Expiry Date (dd-MM-yyyy): ");
+                DateTime licenseExpiryDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
+                Member member1 = new Member(id, firstName, lastName, email, contactNumber, licenseNumber, licenseStartDate, licenseExpiryDate);
+                list.Add(member1);
+            }
+            int j = 1;
+            foreach (var item in list)
+            {
+                Console.WriteLine("Mail {0}: ", j);
+                Console.WriteLine(item.ToString());
+                j++;
+            }
+            if (list[0].Equals(list[1]))
+            {
+                Console.WriteLine("\nMember 1 is same as Member 2");
+            }
+            else
+            {
+                Console.WriteLine("\nMember 1 and Member 2 are different");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
---
+++ Day 11/MailRqmt1/StringSplitDemo/Mail.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplitDemo
+{
+    class Mail
+    {
+        private long _id;
+        public long ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _to;
+        public string To
+        {
+            get { return _to; }
+            set { _to = value; }
+        }
+
+        private string _from;
+        public string From
+        {
+            get { return _from; }
+            set { _from = value; }
+        }
+
+        private string _subject;
+        public string Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        private DateTime _receivedDate;
+        public DateTime ReceivedDate
+        {
+            get { return _receivedDate; }
+            set { _receivedDate = value; }
+        }
+
+        private double _size;
+        public double Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+        public Mail()
+        {
+        }
+        public Mail(long _id, string _to, string _from, string _subject, string _content, DateTime _receivedDate, double _size)
+        {
+            ID = _id;
+            To = _to;
+            From = _from;
+            Subject = _subject;
+            Content = _content;
+            ReceivedDate = _receivedDate;
+            Size = _size;
+        }
+        public override string ToString()
+        {
+            return string.Format("ID:{0}\nTo:{1}\nFrom:{2}\nSubject:{3}\nContent:{4}\nReceivedDate:{5}\nSize:{6}\n",
+                 ID, To, From, Subject, Content, ReceivedDate.ToString("dd-mm-yyyy", null),
+                 Size.ToString("0.0"));
+        }
+        public override bool Equals(object obj)
+        {
+            Mail m1 = obj as Mail;
+            if (m1.To == To && m1.From == From && m1.Subject == Subject)
+                return true;
+            else
+                return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
