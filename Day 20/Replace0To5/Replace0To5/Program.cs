3ebea54883c315c49603ed32a2306cc1b7549705
NabeelaMK <129931417+NabeelaMK@users.noreply.github.com>
2023-04-29 17:28:52 +0530

    Add files via upload

---
+++ Day 20/Replace0To5/Replace0To5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace0To5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a num: ");
+            int num = int.Parse(Console.ReadLine());
+            convertFive();
+        }
+
+        public static int convertFive()
+        {
+            int num = 0;
+            if(num==0)
+            {
+                return 5;
+            }
+            else
+            {
+                int temp = 0;
+                num = num % 10;
+                if(num%10==0)
+                {
+                    return 5;
+                }
+                else
+                {
+                    while(num>0)
+                    temp = temp * 10 + num;
+                    num = num / 10;
+                }
+            }
+            return num;
+        }
+    }
+}
---
+++ Day 20/SwapWithoutTemp/SwapWithoutTemp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapWithoutTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            int b=int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("a= {0}", a);
+            Console.WriteLine("b= {0}\n", b);
+
+        }
+    }
+}
