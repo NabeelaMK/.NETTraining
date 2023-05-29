bc3dbf4b4fe482662331ec7606c38137fe2a5ce1
NabeelaMK <129931417+NabeelaMK@users.noreply.github.com>
2023-05-29 09:25:15 +0530

    Add files via upload

---
+++ Day 42/Fraction/Fraction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int PosCount = 0, NegCount = 0, ZeroCount = 0; 
+            foreach (int num in arr)
+            {
+                if (num > 0)
+                {
+                    PosCount++;
+                }                
+                else if (num < 0)
+                {
+                    NegCount++;
+                }
+                else
+                {
+                    ZeroCount++;
+                }
+            }
+
+            double positiveRatio = (double)PosCount / n;
+            double negativeRatio = (double)NegCount / n;
+            double zeroRatio = (double)ZeroCount / n;
+            Console.WriteLine("Positive Ratio: " + positiveRatio.ToString("F6"));
+            Console.WriteLine("Negative Ratio: " + negativeRatio.ToString("F6"));
+            Console.WriteLine("Zero Ratio: " + zeroRatio.ToString("F6"));
+        }
+    }
+}
---
+++ Day 42/Nabeela/TallestCandle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Result
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int maxHt = 0;
+        int Count = 0;
+        foreach (int candle in candles)
+        {
+            if (candle > maxHt)
+            {
+                maxHt = candle;
+                Count = 1;
+            }
+            else if (candle == maxHt)
+            {
+                Count++;
+            }
+        }
+        return Count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+        int result = Result.birthdayCakeCandles(candles);
+        Console.WriteLine(result);
+    }
+}
