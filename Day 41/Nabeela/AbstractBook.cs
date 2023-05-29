a7f4966ef14e83d7c93e9e24ab8d8b9582710915
NabeelaMK <129931417+NabeelaMK@users.noreply.github.com>
2023-05-29 09:24:33 +0530

    Add files via upload

---
+++ Day 41/Nabeela/AbstractBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookTitle = "A tale of two cities";
+
+            MyBook mybook = new MyBook();
+            mybook.setTitle(bookTitle);
+
+            Console.WriteLine("The title is: " + mybook.getTitle());
+
+        }
+    }
+
+    public abstract class Book
+    {
+        protected string title;
+        public abstract void setTitle(string str);
+
+        public string getTitle()
+        {
+            return title;
+        }
+    }
+
+    class MyBook: Book
+    {
+        public override void setTitle(string str)
+        {
+            title = str;
+        }
+    }
+}
