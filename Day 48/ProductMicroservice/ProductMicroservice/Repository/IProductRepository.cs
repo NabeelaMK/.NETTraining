54f27c406071da408f3c19d9ff72d664d92f4227
NabeelaMK <129931417+NabeelaMK@users.noreply.github.com>
2023-06-05 09:39:19 +0530

    Add files via upload

---
+++ Day 48/ProductMicroservice/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductMicroservice.Models;
+using System.Collections.Generic;
+
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetProductById(int Productid);
+        void InsertProduct(Product Product);
+        void UpdateProduct(Product Product);
+        void DeleteProduct(int Productid);
+        void Save();
+        object GetProducts();
+    }
+}
